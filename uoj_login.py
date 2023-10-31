03c0243ef47f713238073c4975773528cda9fcb5
Ji-Siqi <140613642+Ji-Siqi@users.noreply.github.com>
2023-10-31 14:20:42 +0800

    uoj_login.py

--- uoj_login.py
+++ uoj_login.py
@@ -7,11 +7,11 @@ from hashlib import md5
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0"}
 username = "xf20260003"
 # password=hash.new("FEqAm2pnA6Ed622VqmqLuSKdJ2WJplCT".encode(),"123456789".encode(),"MD5").hexdigest()
-password=72e5fc09b790b841b2c52c50895cf011
+password="72e5fc09b790b841b2c52c50895cf011"
 cxk=md5()
 cxk.update("********".encode())
 # password2=cxk.hexdigest()
-password2=6fe32cb2268c4fc8a548c824ed086ade
+password2="6fe32cb2268c4fc8a548c824ed086ade"
 r1=get("https://www.fzoi.top",headers=headers)
 cookie1=dict_from_cookiejar(r1.cookies)
 r2=get("https://www.fzoi.top/login",headers=headers,cookies=cookie1)
@@ -20,7 +20,7 @@ jntm=search(r"_token : \"([A-Za-z0-9]+)\"",text,M)
 if jntm==None:
     print("Error!")
     exit()
-token=72e5fc09b790b841b2c52c50895cf011
+token="EiSS6clNtNQUftdHVfU7fHvvQ3UcSOGkJwIQ1ehQMmvoByN6qmzSnkFw3SF0"
 r3=post("https://www.fzoi.top/login",cookies=cookie1,headers=headers,data={"username":username,"password":password,"ip":"","_token":token,"response":"","login":"","password2":password2})
 cookie2=dict_from_cookiejar(r3.cookies)
 if cookie2=={}:
